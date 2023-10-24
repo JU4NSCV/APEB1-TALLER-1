e23a250185f57b50121f0921b1fb39f00166e7ec
JU4NSCV <jscueva5@utpl.edu.ec>
2023-10-23 21:45:38 -0500

    primer commit

---
+++ Proyectos en Java/Proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author SEBAS
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básoco de Java Versionado a atraves de Git manejando desde GitHub recuerde usar la sentencia");
+    }
+    
+}
