44968d0dd1cf0a340552f023c692641b77154e75
JU4NSCV <jscueva5@utpl.edu.ec>
2023-10-23 22:01:06 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyectos en Java/proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author SEBAS
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Javaversioado a través de Git manejado desde GitHub además se usar GitHubClassroom");
+    }
+    
+}
